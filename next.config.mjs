71a87355393e8ba115716f8499a5ba9e0e9ec734
jirangi <121839308+jirangi@users.noreply.github.com>
2026-03-14 19:38:53 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',               // 정적 HTML 내보내기 활성화
-  basePath: '/workout_schedule',  // GitHub 레포지토리 이름
-  assetPrefix: '/workout_schedule',
-  images: { unoptimized: true },  // 정적 배포 시 필수
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/workout_schedule" : "",
+  assetPrefix: isProd ? "/workout_schedule/" : "",
 };
 
 export default nextConfig;
